807f9e9e7fcde5485f16024c09c5f0749464ff87
Abhishek <abhishektupudana@gmail.com>
2021-03-25 13:07:29 +0530

    Final Updated

---
+++ JavaPrac/src/com/HelloWorld/SimpleInterestCalculator.java
@@ -0,0 +1,16 @@
+package com.HelloWorld;
+
+public class SimpleInterestCalculator {
+
+	public static void main(String[] args) {
+		
+		int p = 1000;
+		int r = 10;
+		int t = 5;
+		int si = (p * t * r)/100;
+		
+		System.out.println(si);
+
+	}
+
+}
