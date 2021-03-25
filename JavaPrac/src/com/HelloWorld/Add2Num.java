e34937163d2e6a5099441fd1dfdf88dedc9c5378
Abhishek <abhishektupudana@gmail.com>
2021-03-25 11:58:14 +0530

    updated

---
+++ JavaPrac/src/com/HelloWorld/Add2Num.java
@@ -0,0 +1,18 @@
+package com.HelloWorld;
+
+public class Add2Num {
+
+	public static void main(String[] args) {
+		int i = 10;   //4 Bytes space
+		int j = 15;   //4 Bytes space
+		
+		short s = 123;  //2  Bytes space
+		
+		long l = 12345;  //8 Bytes space
+		
+		
+		
+
+	}
+
+}
