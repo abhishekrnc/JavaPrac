299995976b3fba767cc02ae09e46d53f379576f2
Abhishek <abhishektupudana@gmail.com>
2021-03-25 17:53:06 +0530

    Updated the program

--- JavaPrac/src/com/HelloWorld/Add2Num.java
+++ JavaPrac/src/com/HelloWorld/Add2Num.java
@@ -1,33 +1,37 @@
 package com.HelloWorld;
+import java.util.Scanner;         //used to import the scanner datatype
 
 public class Add2Num {
 
 	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);  //Scanner is a datatype and scan is a variable
+		
 		int i = 10;   //4 Bytes space
 		int j = 15;   //4 Bytes space
 		
 		int k = i + j;
 		System.out.println(k);
 		
-		i = 13;     //We have not written here int because we have already created a memory and we are rewriting or reinitializing the value of i in the same memory.
-		
-		
-		
-		short s = 123;  //2  Bytes space
-		
-		long l = 12345;  //8 Bytes space
-		
-		//float f;   //4 Bytes space
-		
-		double d = 1.33;  //8 bytes space
-		
-		char c = 'a';  // 2 Bytes space
-		
-		boolean b = true ;
-		
-		boolean otherB = false;
-		
-		
+//		i = 13;     //We have not written here int because we have already created a memory and we are rewriting or reinitializing the value of i in the same memory.
+//		
+//		
+//		
+//		short s = 123;  //2  Bytes space
+//		
+//		long l = 12345;  //8 Bytes space
+//		
+//		//float f;   //4 Bytes space
+//		
+//		double d = 1.33;  //8 bytes space
+//		
+//		char c = 'a';  // 2 Bytes space
+//		
+//		boolean b = true ;
+//		
+//		boolean otherB = false;
+//		
+//		
 		
 		
 		
