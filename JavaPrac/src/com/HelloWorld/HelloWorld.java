e2cf963c3daf4410c383e654e539568c76cf9183
Abhishek <abhishektupudana@gmail.com>
2021-03-25 11:45:36 +0530

    updated

--- JavaPrac/src/com/HelloWorld/HelloWorld.java
+++ JavaPrac/src/com/HelloWorld/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	
 	public static void main(String args[]) {
 		
+		//If we use println java will put a newline at the end.
+		
 		System.out.println("Hello World this is Abhishek");
 		System.out.print("Hi This is Abhishek");
 		System.out.println("Hello");
