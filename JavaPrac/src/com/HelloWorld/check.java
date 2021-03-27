1573914acfaa6f304378d15dfed6630b443331a3
Abhishek <abhishektupudana@gmail.com>
2021-03-27 22:02:42 +0530

    checking the args statement

---
+++ JavaPrac/src/com/HelloWorld/check.java
@@ -0,0 +1,15 @@
+package com.HelloWorld;
+
+public class check {
+
+	public static void main(String[] args) {
+		int a =10;
+		int b = 12;
+		int c = 1 + b;
+		System.out.println(c);
+		
+	}
+		
+	}
+
+
