eadaf19eb332845692a4bbffab09a63a2aa18033
Abhishek <abhishektupudana@gmail.com>
2021-03-27 22:08:44 +0530

    Updated ASCII

--- JavaPrac/src/com/HelloWorld/DataType.java
+++ JavaPrac/src/com/HelloWorld/DataType.java
@@ -4,8 +4,9 @@ public class DataType {
 
 	public static void main(String t[]) {
 		
-	
-		
+	char c = 'a';
+	System.out.println('a' + 3);         //Here it is taking the ASCII Value for "a". As "+" sign is only defined for the integres.
+										// ASCII value for "a" is 97.
 	}
 		
 	}
