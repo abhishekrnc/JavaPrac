500dccd558938394592d2bfd2f5c25502561e447
Abhishek <abhishektupudana@gmail.com>
2021-03-28 23:24:50 +0530

    updated

--- JavaPrac/src/com/HelloWorld/DataType.java
+++ JavaPrac/src/com/HelloWorld/DataType.java
@@ -5,8 +5,12 @@ public class DataType {
 	public static void main(String t[]) {
 		
 	char c = 'a';
-	System.out.println('a' + 3);         //Here it is taking the ASCII Value for "a". As "+" sign is only defined for the integres.
+	System.out.println(c + 3);         //Here it is taking the ASCII Value for "a". As "+" sign is only defined for the integres.
 										// ASCII value for "a" is 97.
+	
+	int i = c + 10;
+	
+	System.out.println(i);
 	}
 		
 	}
