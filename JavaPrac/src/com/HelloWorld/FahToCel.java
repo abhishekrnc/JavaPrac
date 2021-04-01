28dd6a97eeffa7f942bc0e2ba3d774510c59b51e
Abhishek <abhishektupudana@gmail.com>
2021-04-01 17:01:46 +0530

    updated

--- JavaPrac/src/com/HelloWorld/FahToCel.java
+++ JavaPrac/src/com/HelloWorld/FahToCel.java
@@ -6,9 +6,12 @@ public class FahToCel {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
+		System.out.println("Please enter the Fahrenheit Value");
 		double f = s.nextDouble();
-		double cel = (5/9)*(f - 32);
-		System.out.println(cel);
+		//double cel = (5/9)*(f - 32);
+		
+		double cel = (5 * (f - 32)) / 9;
+		System.out.println(f + " Fahrenheit to Celcius is " + cel);
 
 	}
 
