5b75bb97d0079aa975d7d36e7c420fbdf1d3cdda
Abhishek <abhishektupudana@gmail.com>
2021-04-01 17:04:46 +0530

    updated the code

--- JavaPrac/src/com/HelloWorld/FahToCel.java
+++ JavaPrac/src/com/HelloWorld/FahToCel.java
@@ -10,8 +10,11 @@ public class FahToCel {
 		double f = s.nextDouble();
 		//double cel = (5/9)*(f - 32);
 		
+		int cel1 = (int)((5 * (f - 32)) / 9); //Explict type casting to convert double into int
+		System.out.println(f + " Fahrenheit to Celcius in Integer is " + cel1);
+		
 		double cel = (5 * (f - 32)) / 9;
-		System.out.println(f + " Fahrenheit to Celcius is " + cel);
+		System.out.println(f + " Fahrenheit to Celcius in Double is " + cel);
 
 	}
 
