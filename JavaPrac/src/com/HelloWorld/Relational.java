d89a600ddc86a12c69b2063a7c39718385e8c4bd
Abhishek <abhishektupudana@gmail.com>
2021-04-03 23:44:01 +0530

    updated

--- JavaPrac/src/com/HelloWorld/Relational.java
+++ JavaPrac/src/com/HelloWorld/Relational.java
@@ -1,12 +1,15 @@
 package com.HelloWorld;
 
+import java.util.Scanner;
+
 public class Relational {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int i;
-		int j;
+		int i = s.nextInt();
+		int j = s.nextInt();
 		
+		boolean isEq = (i == j);
 
 	}
 
