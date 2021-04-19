b5156d9ee258c0f230d8271c007705664ad602a2
Abhishek <abhishektupudana@gmail.com>
2021-04-19 22:18:11 +0530

    Updated

--- JavaPrac/src/If_else/LargestOfThreeFormatTwo.java
+++ JavaPrac/src/If_else/LargestOfThreeFormatTwo.java
@@ -1,8 +1,24 @@
 package If_else;
 
+import java.util.Scanner;
+
 public class LargestOfThreeFormatTwo {
 
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		int c = s.nextInt();
+		
+		if (a>=b && a>=c) {
+			System.out.println("A=");
+			System.out.println(a);
+		}
+		else
+			if(b>=c && b>=a) {
+				System.out.println("B=");
+				System.out.println(b);
+			}
 	}
 
 }
