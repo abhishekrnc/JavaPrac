20b88ed3f8dbdc102d98fa984b37687b26913ef1
Abhishek <abhishektupudana@gmail.com>
2021-04-19 22:27:13 +0530

    Added a new file

---
+++ JavaPrac/src/If_else/IntToString.java
@@ -0,0 +1,21 @@
+package If_else;
+
+import java.util.Scanner;
+
+public class IntToString {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		if (a == 1) {
+			System.out.println("One");
+		}
+		if (a == 2) {
+			System.out.println("Two");
+		}
+		if (a == 3) {
+			System.out.println("Three");
+		}
+	}
+
+}
