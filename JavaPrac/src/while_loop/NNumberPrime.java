2559ab1202c005de59008c8c37d03f4c8484d74b
Abhishek <abhishektupudana@gmail.com>
2021-04-20 23:09:24 +0530

    Added a new file

---
+++ JavaPrac/src/while_loop/NNumberPrime.java
@@ -0,0 +1,14 @@
+package while_loop;
+
+import java.util.Scanner;
+
+public class NNumberPrime {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int d = 2;
+		
+	}
+
+}
