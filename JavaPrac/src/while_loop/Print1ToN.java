7de1552014468c332dcc92beff3a3997839c795c
Abhishek <abhishektupudana@gmail.com>
2021-04-20 22:38:59 +0530

    Updated

--- JavaPrac/src/while_loop/Print1ToN.java
+++ JavaPrac/src/while_loop/Print1ToN.java
@@ -1,9 +1,12 @@
 package while_loop;
 
+import java.util.Scanner;
+
 public class Print1ToN {
 
 	public static void main(String[] args) {
-		
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
 	}
 
 }
