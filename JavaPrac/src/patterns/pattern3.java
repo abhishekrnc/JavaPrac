73b001f019c0a8ebf826ea883f470bd27e20300c
Abhishek <abhishektupudana@gmail.com>
2021-04-23 22:37:54 +0530

    updated

--- JavaPrac/src/patterns/pattern3.java
+++ JavaPrac/src/patterns/pattern3.java
@@ -1,8 +1,20 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class pattern3 {
 
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i = 1;
+		int val = 1;
+		while (i <= n) {
+			int space = 1;
+			while (space <= n -i) {
+				System.out.print(" ");
+			}
+		}
 	}
 
 }
